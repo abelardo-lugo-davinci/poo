3828a052fb686e8049b1316f535fd1a57d68fb2c
Abelardo Lugo <abelardo.lugo@davinci.edu.ar>
2021-11-04 12:44:00 -0300

    Modificadores de acceso, UML, palabra reservadas static y final

---
+++ clase21/src/figura3d/FiguraTridimensional.java
@@ -0,0 +1,13 @@
+package figura3d;
+
+public abstract class FiguraTridimensional {
+
+    // CONSTANTE
+    protected static final int DIMENSIONES = 3;
+
+    // METODOS ABSTRACTOS
+
+    public abstract double calcularVolumen();
+    public abstract double calcularArea();
+
+}
